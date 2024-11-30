61b7e93bf17191f7bd97d50bf5a2941d90631b31
Ghavio Rizky <ghavio1516@gmail.com>
2024-11-30 12:51:01 +0700

    penyesuaian timeout

--- app/src/main/java/com/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/data/retrofit/ApiConfig.kt
@@ -3,16 +3,22 @@ package com.data.retrofit
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
-
     fun getApiService(): ApiService {
-        val client = OkHttpClient.Builder().build()
+        val client = OkHttpClient.Builder()
+            .connectTimeout(120, TimeUnit.SECONDS) // Timeout untuk koneksi
+            .readTimeout(120, TimeUnit.SECONDS)  // Timeout untuk membaca data
+            .writeTimeout(120, TimeUnit.SECONDS) // Timeout untuk menulis data
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://nutrigood.akmalnurwahid.my.id") // URL backend
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
+
         return retrofit.create(ApiService::class.java)
     }
 }
